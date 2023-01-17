e71a2e64994091262b5924f87f9a8b3db1ab345e
sarvarbek <abduqodirovsarvar.2002@gmail.com>
2023-01-17 18:45:14 +0500

    Added Enums

---
+++ Domain/Enums/Payments.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Proj.Domain.Enums
+{
+    public enum Payments
+    {
+        Plastic_card,
+        Cash
+    }
+}
---
+++ Domain/Enums/UserRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Proj.Domain.Enums
+{
+    public enum UserRole
+    {
+        user,
+        admin,
+        superAdmin
+    }
+}
